cd54b38f36d90ec541f59fd56189b967517cf9b7
Dana Maraqa <dana.maraqa@hotmail.com>
2023-11-21 21:27:25 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
 // Write your solution in this file!
 const employee = {
-    name: 'Sam',
-    streetAddress: '123 Main Street',
-  };
-  
-  function updateEmployeeWithKeyAndValue(employee, key, value) {
-    return { ...employee, [key]: value };
-  }
-  
-  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key] = value;
-    return employee;
-  }
-  
-  function deleteFromEmployeeByKey(employee, key) {
-    const newEmployee = { ...employee };
+  name :'Sam',
+  streetAddress : '11 Broadway',
+};
+
+
+function updateEmployeeWithKeyAndValue(employee,key,value) {
+  return {...employee,[key]: value};
+};
+
+  const employeeNewAddress = updateEmployeeWithKeyAndValue(employee,'streetAddress','12 Broadway')
+  console.log(employeeNewAddress);
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value) {
+  employee[key]= value ;
+
+  return employee;
+
+  const employeeNewAddressDes = destructivelyUpdateEmployeeWithKeyAndValue(employee,'streetAddress','12 Broadway');
+  console.log(employeeNewAddressDes);
+};
+
+function deleteFromEmployeeByKey(employee,key,value) {
+
+    const newEmployee = {...employee};
     delete newEmployee[key];
     return newEmployee;
   }
   
-  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  const deletedKey = deleteFromEmployeeByKey(employee,'name')
+
+  console.log(deletedKey);
+
+  function  destructivelyDeleteFromEmployeeByKey(employee, key) {
+    
     delete employee[key];
+
     return employee;
-  }
-  
- 
+    
+  };
+
+  const deletedKeyDes = destructivelyDeleteFromEmployeeByKey(employee,'name');
